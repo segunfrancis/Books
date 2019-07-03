1127b1616927e2dd7cbe1d32ebb015cf8d738f33
segunfrancis <francis4dblues@gmail.com>
2019-07-03 15:37:26 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/books/ApiUtil.java
@@ -0,0 +1,98 @@
+package com.example.books;
+
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ApiUtil {
+
+    // Removing the constructor
+    public ApiUtil() {
+    }
+
+    public static final String BASE_API_URL = "https://www.googleapis.com/books/v1/volumes";
+    public static final String QUERY_PARAMETER_KEY = "q";
+    public static final String KEY = "key";
+    public static final String API_KEY = "AIzaSyBQnkpj7Ucbzb1SmFciJFHU2S5-w9vYg2w";
+
+    public static URL buildUrl(String title) {
+        URL url = null;
+        Uri uri = Uri.parse(BASE_API_URL).buildUpon()
+                .appendQueryParameter(QUERY_PARAMETER_KEY, title)
+                .appendQueryParameter(KEY, API_KEY)
+                .build();
+        try {
+            url = new URL(uri.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+    // Connecting to the API
+    public static String getJson(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        try {
+            InputStream stream = connection.getInputStream();
+            Scanner scanner = new Scanner(stream);
+            scanner.useDelimiter("\\A");
+            boolean hasData = scanner.hasNext();
+            if (hasData) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            Log.d("Error", e.toString());
+            return null;
+        } finally {
+            connection.disconnect();
+        }
+    }
+
+    public static ArrayList<Book> getBooksFromJson(String json) {
+
+        final String ID = "id";
+        final String TITLE = "title";
+        final String SUBTITLE = "subtitle";
+        final String AUTHORS = "authors";
+        final String PUBLISHER = "publisher";
+        final String PUBLISHED_DATE = "publishedDate";
+        final String ITEMS = "items";
+        final String VOLUME_INFO = "volumeInfo";
+
+        ArrayList<Book> books = new ArrayList<>();
+        try {
+            JSONObject jsonBooks = new JSONObject(json);
+            JSONArray arrayBooks = jsonBooks.getJSONArray(ITEMS);
+            int numberOfBooks = arrayBooks.length();
+            for (int i = 0; i < numberOfBooks; i++) {
+                JSONObject bookJSON = arrayBooks.getJSONObject(i);
+                JSONObject volumeInfoJSON = bookJSON.getJSONObject(VOLUME_INFO);
+                int authorNumber = volumeInfoJSON.getJSONArray(AUTHORS).length();
+                String[] authors = new String[authorNumber];
+                for (int j = 0; j < authorNumber; j++) {
+                    authors[j] = volumeInfoJSON.getJSONArray(AUTHORS).get(j).toString();
+                }
+                Book book = new Book(bookJSON.getString(ID), volumeInfoJSON.getString(TITLE),
+                        (volumeInfoJSON.isNull(SUBTITLE) ? "" : volumeInfoJSON.getString(SUBTITLE)),
+                        authors, volumeInfoJSON.getString(PUBLISHER), volumeInfoJSON.getString(PUBLISHED_DATE));
+                books.add(book);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return books;
+    }
+}
---
+++ app/src/main/java/com/example/books/BookListActivity.java
@@ -0,0 +1,73 @@
+package com.example.books;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class BookListActivity extends AppCompatActivity {
+
+    private ProgressBar mLoadingProgress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_list);
+        mLoadingProgress = findViewById(R.id.pb_loading);
+
+        try {
+            URL bookUrl = ApiUtil.buildUrl("cooking");
+            new BooksQueryTask().execute(bookUrl);
+        } catch (Exception e) {
+            Log.d("error", e.getMessage());
+        }
+    }
+
+    public class BooksQueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchURL = urls[0];
+            String result = null;
+            try {
+                result = ApiUtil.getJson(searchURL);
+            } catch (IOException e) {
+                Log.e("Error", e.getMessage());
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            TextView tvResult = findViewById(R.id.tvResponse);
+            TextView tvError = findViewById(R.id.tv_error);
+            mLoadingProgress.setVisibility(View.INVISIBLE);
+            if (result == null) {
+                tvResult.setVisibility(View.INVISIBLE);
+                tvError.setVisibility(View.VISIBLE);
+            } else {
+                tvResult.setVisibility(View.VISIBLE);
+                tvError.setVisibility(View.INVISIBLE);
+            }
+            ArrayList<Book> books = ApiUtil.getBooksFromJson(result);
+            String resultString = "";
+            for (Book book : books) {
+                resultString = resultString + book.title + "\n" + book.publishedDate + "\n\n";
+            }
+            tvResult.setText(resultString);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingProgress.setVisibility(View.VISIBLE);
+        }
+    }
+}
